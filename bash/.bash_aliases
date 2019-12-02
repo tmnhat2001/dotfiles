ece33af933b2b12164397cffc1af49912c6a2939
James Tran <tmnhat2001@gmail.com>
2019-12-01 21:43:03 -0500

    Initial commit

---
+++ bash/.bash_aliases
@@ -0,0 +1,1 @@
+alias ruby-build-upgrade='cd "$(rbenv root)"/plugins/ruby-build && git pull'
---
+++ setup_linux.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+packages=(
+  build-essential
+  curl
+  git
+  htop
+  rbenv
+  redis
+  zsh
+)
+
+for pkg in "${packages[@]}"
+do
+  sudo apt install "${pkg}"
+done
+
+# Install ruby-build
+mkdir -p "$(rbenv root)"/plugins
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+
+# Install Chrome
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# Link dotfiles
+
+ln -sf ~/dotfiles/bash/.bash_aliases ~/.bash_aliases
+ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
